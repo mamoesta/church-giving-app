d3743051a132f3cb488125bf4fe75e16377a9ecc
mamoesta <31963563+mamoesta@users.noreply.github.com>
2017-12-02 16:34:41 -0500

    no message

--- server.js
+++ server.js
@@ -32,8 +32,10 @@ app.listen(3333, function (){
     console.log('Server started');
 })
 */
-var express = require('express');
-var app = express();
-
-app.use(express.static(__dirname + "/www"));
-app.listen(process.env.PORT || 5000);
+var express = require('express'),
+app = express();
+app.use(express.static('www'));
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
